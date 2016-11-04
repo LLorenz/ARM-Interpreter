cb81721a0eabc2b18b28afb98d0ab82bbaf46b82
Elias Holzmann <bartim@users.noreply.github.com>
2016-11-04 16:04:09 +0100

    Added base architecture for command parsing and simple arithmetics (ADD, SUB). Currently untested.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+const MAX_INTEGER = Math.pow(2, 32);
+
+function assert(bool) {
+	if (!bool) {
+		console.trace("Error in assertion");
+	}
+}
+
+function getNthBit(bit, number) {
+	return (number >> bit) % 2;
+}
+
 var registers = [];
 for (var i = 0; i < 16; i++) {
 	registers.push(0);
@@ -10,3 +22,65 @@ var flags = {
 	OVERFLOW: false,
 	PARITY: false
 }
+
+//IIFE for scoping reasons
+var commandTree = (function() {
+	var commandList = [
+	];
+
+	function arith(arithOperator, writeStatus, result, firstOp, secondOp) {
+		assert(arguments.length == 5);
+		result = getRegisterFromArgument(result);
+		firstOp = registers[getRegisterFromArgument(firstOp)];
+		secondOp = evalFlexibleOperator(secondOp);
+		result = arithOperator(writeStatus, firstOp, secondOp);
+	}
+
+	[
+		["ADD", function(writeStatus, first, second) {
+			var result = (first + second) % MAX_INTEGER;
+			if (writeStatus) {
+				flags.CARRY = (result < first);
+				flags.ZERO = !result;
+				flags.NEGATIVE = getNthBit(31, result % MAX_INTEGER);
+				/*
+				 * Overflow if signed bits are
+				 *
+				 * first | second | result
+				 * ------+--------+-------
+				 *   0   |    0   |   1
+				 *   0   |    1   | never
+				 *   1   |    0   | never
+				 *   1   |    1   |   0
+				 */
+				flags.OVERFLOW = getNthBit(31, first) == getNthBit(31, second) && getNthBit(31, first) != getNthBit(31, result);
+			}
+			return result;
+		}],
+		["SUB", function(writeStatus, first, second) {
+			var result = (first + second) % MAX_INTEGER;
+			if (writeStatus) {
+				flags.CARRY = (result <= first); // ARM Architecture Reference Manual, p 50
+				flags.ZERO = !result;
+				flags.NEGATIVE = getNthBit(31, result % MAX_INTEGER);
+				/*
+				 * Overflow if signed bits are
+				 *
+				 * first | second | result
+				 * ------+--------+-------
+				 *   0   |    0   | never
+				 *   0   |    1   |   1
+				 *   1   |    0   |   0
+				 *   1   |    1   | never
+				 */
+				flags.OVERFLOW = getNthBit(31, first) != getNthBit(31, second) && getNthBit(31, first) != getNthBit(31, result);
+			}
+			return result;
+		}]
+	].forEach(function(array) {
+		var newCommandListElement = [array[0] + "<cond><S>", arith.bind(array[1])];
+		commandList.push(newCommandListElement);
+	});
+
+	// TODO add transformation from list to tree
+}());
